990fc2aa325a25aa3d196656cd4f6839bd8db010
ErnarGabbasov <125185114+ErnarGabbasov@users.noreply.github.com>
2023-05-15 18:58:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def find_word():
             filename = file.filename  # Получение имени файла
             if filename.endswith('.docx'):  # Если файл формата .docx
                 doc = docx.Document(file)  # Создание объекта документа Word из файла
-                words = []
+                words = [] # список слов
                 for para in doc.paragraphs:  # Для каждого абзаца в документе
                     words.extend(para.text.split())  # Добавление слов из абзаца в список слов
             elif filename.endswith('.txt'):  # Если файл формата .txt
@@ -24,7 +24,7 @@ def find_word():
             else:
                 return render_template('upload_file.html', error='Поддерживаются только файлы формата .docx и .txt') # Отображение сообщения об ошибке, если файл не допустимого формата
             words = [re.sub(r'[^\w\s]', '', word) for word in words if re.search(r'[a-zA-Zа-яА-Я]', word)] # Удаляем знаки препинания
-            word_counts = {}
+            word_counts = {} # счетчик для слова
             for word in words:  # Для каждого слова в списке слов
                 if word in word_counts:
                     word_counts[word] += 1  # Увеличение счетчика для этого слова
@@ -33,7 +33,7 @@ def find_word():
             most_common_word = max(word_counts, key=word_counts.get)  # Нахождение слова с максимальным количеством повторений в словаре word_counts и присвоение его переменной most_common_word.
             return render_template('result.html', most_common_word=most_common_word)  # Отображение "result.html" и передача самого часто встречающегося слова в качестве аргумента.
         else:
-            return render_template('upload_file.html', error='Поддерживаются только файлы формата .docx и .txt')
+            return render_template('upload_file.html', error='Поддерживаются только файлы формата .docx и .txt') # если загруженный файл не является допустимым, отображается шаблон "upload.html" с сообщением об ошибке.
     return render_template('upload_file.html')
 
 def allowed_file(filename):  # Функция, которая проверяет, является ли расширение загруженного файла допустимым.
